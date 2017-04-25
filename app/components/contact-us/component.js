31bf2b3b6c49cfc406ef2c651a60ed90f4ccc20f
Nathan Hill <nathan.hill@inin.com>
2017-04-25 01:21:44 -0400

    Added a message for when the contact form is sent successfully.

--- app/components/contact-us/component.js
+++ app/components/contact-us/component.js
@@ -16,7 +16,7 @@ export default Ember.Component.extend({
     editMode: Ember.computed('sending', 'sentSuccessfully', 'sentUnsuccessfully', function() {
         return !(this.get('sending') || this.get('sentSuccessfully') || this.get('sentUnsuccessfully'));
     }),
-    // sentSuccessfully: true,
+    sentSuccessfully: true,
     actions: {
         send() {
             this.set('sending', true);
