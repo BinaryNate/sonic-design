18e1c7918438c69c2199b7b0007a33736eb95bc1
Nathan Hill <nathan.hill@inin.com>
2017-04-25 00:35:52 -0400

    I now have form validation and a loading spinner working in the contact form, but still need to include the actual API request as well as error handling.

--- app/components/contact-us/component.js
+++ app/components/contact-us/component.js
@@ -1,5 +1,29 @@
 import Ember from 'ember';
 
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const delay = ms => new Ember.RSVP.Promise((res) => setTimeout(res, ms));
+
 export default Ember.Component.extend({
-    ajax: Ember.inject.service()
+    ajax: Ember.inject.service(),
+
+    emailIsValid: Ember.computed('email', function() { return EMAIL_REGEX.test(this.get('email')); }),
+    messageIsValid: Ember.computed('message', function() {
+        let message = this.get('message');
+        return (typeof message === 'string') && message.trim();
+    }),
+    readyToSend: Ember.computed.and('emailIsValid', 'messageIsValid'),
+    actions: {
+        send() {
+            this.set('sending', true);
+            return delay(2000)
+            .then(() => this.set('sending', false));
+            // let data = {
+            //     email: this.get('email'),
+            //     message: this.get('message')
+            // };
+
+            // return this.get('ajax').post('/api/v2/contactMessages', { data })
+        }
+    }
 });
